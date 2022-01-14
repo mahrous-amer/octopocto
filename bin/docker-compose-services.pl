241d931d865b44d2f16f48165d33357d49d7dd13
Nael Alolwani <naelalolwani@gmail.com>
2022-01-15 02:27:32 +0800

    init docker compose and redis clusters

---
+++ bin/docker-compose-services.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/env perl 
+use strict;
+use warnings;
+
+=pod
+
+Populates the C<docker-compose.yml> file with services from the C<services/> directory,
+expecting them to be defined as C<category/name> with a C<config.yml>
+there will be a Redis cluster for every category.
+In config there are some rquired
+
+- language:
+
+As for every language C<base/language/> settigs will be included.
+
+=cut
+
+use Template;
+use Path::Tiny;
+use YAML::XS qw(LoadFile);
+use List::UtilsBy qw(extract_by);
+
+use Log::Any qw($log);
+use Log::Any::Adapter qw(Stderr), log_level => 'info';
+
+my %default_env = (
+    CLUSTER            => "1",
+    TRANSPORT          => "redis://redis-node-0:6379",
+    LOG_LEVEL          => "info",
+    LIBRARY            => "/base/",
+);
+
+my $service_dir = path('services');
+my @cat_dirs = grep { $_->is_dir } $service_dir->children;
+my ( @services, @categories );
+for my $category (@cat_dirs) {
+    # per category
+    push @categories, {name => $category->basename};
+
+
+    for my $srv (grep { $_->is_dir } $category->children) {
+        my $config_path = $srv->child('config.yml');
+        my %args;
+        my $path = join '/', 'services', $category->basename, $srv->basename;
+        if($srv->child('Dockerfile')->exists) {
+            $args{build} = $path;
+        } else {
+            $args{image} = 'perl-vv-framework';
+        }
+        # pg node name
+        $args{pg} = join('-', $category->basename, $srv->basename);
+        
+        my @service_dir = $srv->child('lib/Service/')->children;
+        # At the moment only one Service should be present.
+        my ($service_name) = $service_dir[0] =~ /.*\/(.*).pm/m;
+        $args{environment}{SERVICE_NAME} = "Service::$service_name";
+        $args{environment}{APP} = $category->basename;
+        $args{environment}{DATABASE} = $args{pg};
+
+        # Include additional environment variables.
+        @{$args{environment}}{keys %default_env} = values %default_env;
+        my $env = $srv->child('.env');
+        if ( $env->is_file ) {
+            my @env = $env->lines_utf8({ chomp => 1 });
+            my %env_hash = map { split /=/, $_, 2 } @env;
+            @{$args{environment}}{keys %env_hash} = values %env_hash;
+        }
+
+
+        $args{volumes} = ["./$path:/app/", './pg_service.conf:/root/.pg_service.conf:ro'];
+        $args{ports} = ["80:80"] if $srv->basename eq 'api';
+        if($config_path->exists) {
+            my $cfg = LoadFile($config_path);
+            push @services, {
+                %args,
+                instance => join('_', $category->basename, $_),
+                name     => $srv->basename,
+                category => $category->basename,
+            } for sort keys +($cfg->{instances} || {})->%*;
+        } else {
+            push @services, {
+                %args,
+                instance => join('_', $category->basename, $srv->basename),
+                name     => $srv->basename,
+                category => $category->basename,
+            }
+        }
+    }
+}
+
+
+$log->tracef('- %s', $_) for @services;
+$log->infof('%d total categories defined', 0 + @categories);
+$log->infof('%d total services defined', 0 + @services);
+
+my $tt = Template->new;
+$tt->process(
+    'templates/docker-compose.yml.tt2',
+    {
+        service_list => \@services,
+        category_list => \@categories,
+    },
+    'docker-compose.yml'
+) or die $tt->error;
+
